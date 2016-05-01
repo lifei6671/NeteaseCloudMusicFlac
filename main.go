5819b204b3e6ba2af1a698cb6f8439018e4f184c
李飞麟 <longfei6671@163.com>
2016-05-01 22:17:14 +0800

    优化代码

--- main.go
+++ main.go
@@ -180,8 +180,8 @@ func DownloadString(remoteUrl string,queryValues url.Values) (body []byte,err er
 	reqest.Header.Add("Accept-Encoding", "gzip, deflate");
 	reqest.Header.Add("Accept-Language", "zh-cn,zh;q=0.8,en-us;q=0.5,en;q=0.3");
 	reqest.Header.Add("Connection", "keep-alive");
-	reqest.Header.Add("Host", "login.sina.com.cn");
-	reqest.Header.Add("Referer", uri.Host);
+	reqest.Header.Add("Host", uri.Host);
+	reqest.Header.Add("Referer", uri.String());
 	reqest.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:12.0) Gecko/20100101 Firefox/12.0");
 
 	response, err := client.Do(reqest)
