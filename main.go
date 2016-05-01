2cfaaa0d1a1bea1210393c97256943e424366a3f
李飞麟 <longfei6671@163.com>
2016-05-01 22:16:04 +0800

    优化代码

--- main.go
+++ main.go
@@ -11,12 +11,15 @@ import (
 	"encoding/json"
 	"sync"
 	"io"
+	"compress/gzip"
 )
 
 const(
 	SuggestionUrl = "http://sug.music.baidu.com/info/suggestion";
 	Fmlink = "http://music.baidu.com/data/music/fmlink";
 )
+
+
 func main()  {
 	if(len(os.Args) <= 1){
 		fmt.Println("请输入网易音乐链接.");
@@ -26,24 +29,12 @@ func main()  {
 
 	nurl := strings.Replace(os.Args[1],"#/","",-1);
 
-	uri,err := url.Parse(nurl);
-
-	if(err != nil){
-		fmt.Println("解析URL时出错：",err);
-		return ;
-	}
-	response,err := http.Get(uri.String());
+	response,err := DownloadString(nurl,nil);
 	if(err != nil){
 		fmt.Println("获取远程URL内容时出错：",err);
 		return ;
 	}
 
-	responseBody,err := ioutil.ReadAll(response.Body);
-	response.Body.Close();
-	if(err != nil){
-		fmt.Println("读取远程URL响应内容时出错：",err);
-		return ;
-	}
 
 	var path string;
 
@@ -66,7 +57,7 @@ func main()  {
 
 	reg := regexp.MustCompile(`<ul class="f-hide">(.*?)</ul>`);
 
-	mm := reg.FindAllString(string(responseBody),-1);
+	mm := reg.FindAllString(string(response),-1);
 
 	waitGroup := sync.WaitGroup{};
 
@@ -79,30 +70,21 @@ func main()  {
 
 		for _,item := range urlli{
 
-			murl,_ := url.Parse(SuggestionUrl);
-
-			query := murl.Query();
+			query := url.Values{};
 			query.Set("word", string(item[1]));
 			query.Set("version","2");
 			query.Set("from","0");
 
-			murl.RawQuery = query.Encode();
+			res,err := DownloadString(SuggestionUrl,query);
+			if(err != nil){
+				fmt.Println("获取音乐列表时出错：",err);
+				continue;
+			}
 
-			res,err := http.Get(murl.String());
-			if(err != nil){
-				fmt.Println("获取",murl,"出错：",err);
-				continue;
-			}
-			content,err := ioutil.ReadAll(res.Body);
-			res.Body.Close();
-			if(err != nil){
-				fmt.Println("解析",murl,"响应值时出错：",err);
-				continue;
-			}
 
 			var dat map[string]interface{};
 
-			err = json.Unmarshal(content, &dat);
+			err = json.Unmarshal([]byte(res), &dat);
 
 			if err != nil {
 				fmt.Println("反序列化JSON时出错:",err);
@@ -116,35 +98,23 @@ func main()  {
 
 			songid := dat["data"].(map[string]interface{})["song"].([]interface{})[0].(map[string]interface{})["songid"].(string);
 
-			link ,err:= url.Parse(Fmlink);
-			if(err != nil){
-				fmt.Println("解析音乐链接时出错：",err);
-				continue;
-			}
-			query = link.Query();
+			query = url.Values{};
 			query.Set("songIds",songid);
 			query.Set("type","flac");
-			link.RawQuery = query.Encode();
-			res ,err = http.Get(link.String());
+
+			res ,err = DownloadString(Fmlink,query);
 
 			if(err != nil){
 				fmt.Println("获取音乐文件时出错：",err);
 				continue;
 			}
 
-			content,err = ioutil.ReadAll(res.Body);
-			res.Body.Close();
-			if(err != nil){
-				fmt.Println("读取音乐文件数据时出错：",err);
-				continue;
-			}
-
 			var data map[string]interface{};
 
-			err = json.Unmarshal(content,&data);
+			err = json.Unmarshal(res,&data);
 
 			if code,ok:= data["errorCode"]; ((ok && code.(float64) == 22005) || err != nil){
-				fmt.Println("解析音乐文件时出错：",err,string(content));
+				fmt.Println("解析音乐文件时出错：",err);
 				continue;
 			}
 
@@ -152,7 +122,7 @@ func main()  {
 
 			r := []rune(songlink)
 			if(len(r) < 10){
-				fmt.Println("没有无损音乐地址");
+				fmt.Println("没有无损音乐地址:",string(item[1]));
 				continue;
 			}
 
@@ -164,12 +134,12 @@ func main()  {
 
 			waitGroup.Add(1);
 			go func() {
-				fmt.Println(songname , " is downloading now ......");
+				fmt.Println("正在下载 ", songname," ......");
+				defer waitGroup.Done();
 
 				songRes ,err:= http.Get(songlink);
 				if(err != nil){
 					fmt.Println("下载文件时出错：",songlink);
-					waitGroup.Done();
 					return ;
 				}
 
@@ -177,15 +147,70 @@ func main()  {
 				written,err := io.Copy(songFile,songRes.Body);
 				if(err != nil){
 					fmt.Println("保存音乐文件时出错：",err);
-					waitGroup.Done();
 					return ;
 				}
-				fmt.Println("下载",filename,"完成,文件大小：",fmt.Sprintf("%.2f", (float64(written)/(1024*1024))),"MB");
-				waitGroup.Done();
+				fmt.Println(songname,"下载完成,文件大小：",fmt.Sprintf("%.2f", (float64(written)/(1024*1024))),"MB");
 			}();
 
 		}
 
 	}
 	waitGroup.Wait();
+}
+
+
+func DownloadString(remoteUrl string,queryValues url.Values) (body []byte,err error){
+	client := &http.Client{};
+	body = nil;
+	uri,err := url.Parse(remoteUrl);
+	if(err != nil){
+		return ;
+	}
+	if(queryValues != nil){
+		values := uri.Query();
+		if(values != nil){
+			for k,v := range values {
+				queryValues[k] = v;
+			}
+		}
+		uri.RawQuery = queryValues.Encode();
+	}
+	reqest, err := http.NewRequest("GET",uri.String(),nil);
+	reqest.Header.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+	reqest.Header.Add("Accept-Encoding", "gzip, deflate");
+	reqest.Header.Add("Accept-Language", "zh-cn,zh;q=0.8,en-us;q=0.5,en;q=0.3");
+	reqest.Header.Add("Connection", "keep-alive");
+	reqest.Header.Add("Host", "login.sina.com.cn");
+	reqest.Header.Add("Referer", uri.Host);
+	reqest.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:12.0) Gecko/20100101 Firefox/12.0");
+
+	response, err := client.Do(reqest)
+	defer response.Body.Close();
+	if(err != nil){
+		return ;
+	}
+
+	if response.StatusCode == 200 {
+		switch response.Header.Get("Content-Encoding") {
+		case "gzip":
+			reader, _ := gzip.NewReader(response.Body)
+			for {
+				buf := make([]byte, 1024)
+				n, err := reader.Read(buf)
+
+				if err != nil && err != io.EOF {
+					panic(err)
+				}
+
+				if n == 0 {
+					break
+				}
+				body = append(body,buf...);
+			}
+		default:
+			body, _ = ioutil.ReadAll(response.Body)
+
+		}
+	}
+	return ;
 }
